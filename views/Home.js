5ca776b9039acf3d57d409ef610dc088b3dcef13
heyitsuzair <uzairdevil354123@gmail.com>
2022-09-18 01:57:13 +0500

    Hero

--- views/Home.js
+++ views/Home.js
@@ -1,13 +1,20 @@
-import {View, Text} from 'react-native';
+import {View, Image, StyleSheet, Dimensions, Text} from 'react-native';
 import React from 'react';
 
 export default function Home() {
   return (
     <View>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
+      <Image
+        source={require('../assets/images/deal1.png')}
+        style={styles.img}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  img: {
+    width: Dimensions.get('window').width,
+    resizeMode: 'cover',
+  },
+});
