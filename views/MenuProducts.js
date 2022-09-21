375a7cb04b3ac9b0b8f9298e0dcb66eac95917e0
heyitsuzair <uzairdevil354123@gmail.com>
2022-09-21 23:53:22 +0500

    Midnight Validation Added

--- views/MenuProducts.js
+++ views/MenuProducts.js
@@ -1,13 +1,25 @@
-import {StyleSheet, ScrollView, View, Dimensions} from 'react-native';
-import React, {useEffect} from 'react';
+import {StyleSheet, ScrollView, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import Card from '../components/Cards/Product';
+import {Toast} from 'toastify-react-native';
 
 export default function MenuProducts({route, navigation}) {
+  const checkIfMidnight = () => {
+    if (route.params.title === 'Midnight') {
+      const getHour = new Date().getHours();
+      getHour > 2 ? navigation.goBack() : '';
+      Toast.warn('Available From 12AM To 2AM Only!');
+    } else {
+      return;
+    }
+  };
+
   useEffect(() => {
     // changing header title according to the title incoming
     navigation.setOptions({
       title: route.params.title,
     });
+    checkIfMidnight();
     //eslint-disable-next-line
   }, []);
 
